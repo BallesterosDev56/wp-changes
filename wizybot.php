b6aed59a67a3d19cb244ffd265601f4b33e8a574
BallesterosDev56 <ballesterosdeveloper56@gmail.com>
2026-05-07 10:11:25 -0500

    Add Domain Path to plugin header for localization support

--- wizybot.php
+++ wizybot.php
@@ -10,6 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Author:            Wizybot Inc
  * Author URI:        https://wizybot.com
  * Text Domain:       wizybot
+ * Domain Path:       /languages
  * License:           GPL v2 or later
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Requires at least: 5.2
